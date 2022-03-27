6aaac73be3661ef63ae1f01bd46f013c7736d7f3
Corentin goetghebeur <corentin.goetghebeur@ensta-bretagne.org>
2022-03-27 17:45:03 +0200

    finished template + changed colors

--- app.js
+++ app.js
@@ -1,47 +1,45 @@
-// Récupération des éléments dans la page html
 const sections = document.querySelectorAll('.section');
-const sectionButtons = document.querySelectorAll(".controls");
-const sectionButton = document.querySelectorAll(".control");
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
 
-const allSections = document.querySelector(".main-content");
 
-// Fonction de transition des pages quand on clique su les boutons.
-function pageTransition(){
-    // Button click active class
-    for(let i = 0; i < sectionButton.length; i++){
-        sectionButton[i].addEventListener('click', function() {
+function PageTransitions(){
+    //Button click active class
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
-
-            // replace active with empty in the class name
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
             this.className += ' active-btn';
         })
     }
 
-    // Sections active class
-    allSections.addEventListener('click', function(e) {
+    //Sctions Active 
+    allSections.addEventListener('click', (e) =>{
         const id = e.target.dataset.id;
         if(id){
-            //remove selected from the other buttons
-            sectionButtons.forEach((btn) => {
-                btn.classList.remove('active');
-
+            //resmove selected from the other btns
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
             })
-            // show the selected section
-            e.target.classList.add('active');
-            
-            //hide the other sections
+            e.target.classList.add('active')
+
+            //hide other sections
             sections.forEach((section)=>{
-                section.classList.remove('active');
+                section.classList.remove('active')
             })
 
             const element = document.getElementById(id);
             element.classList.add('active');
-
         }
-        console.log(e.target);
     })
-    
-   
+
+    //Toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',() =>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 }
-pageTransition();
+
+PageTransitions();
